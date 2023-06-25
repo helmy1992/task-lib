9c5428dc3c449f5f7feb3df658912b3138ff9bc0
HELMY <helmy@ubuntu.myguest.virtualbox.org>
2023-06-25 00:55:47 -0700

    five

---
+++ vars/deploy.groovy
@@ -0,0 +1,4 @@
+def call() {
+  echo "the code is deploying" 
+  echo "this build number $BUILD_NUMBER"
+}
---
+++ vars/test.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+def call() {
+  echo "now your app is testing " 
+}
